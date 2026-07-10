cf531dae48216b1fcd7b18e202a8cd13654bd32c
scDjanGo <scdjango229101@gmail.com>
2026-07-10 18:02:05 +0600

    home work 10.07.2026

--- script.js
+++ script.js
@@ -1,65 +1,97 @@
-
-
-
-
-
-
-
-
-let date = new Date()
-
-
-
-// console.log(date);
-// console.log(date.getTime())
-
-// date.setFullYear(2023)
-// date.setMonth(24)
-// date.setDate(370)
-// date.setHours(20)
-// date.setMinutes(80)
-// date.setSeconds(90)
-// date.setMilliseconds(9990)
-
-
-
-
-
-console.log(date.getMilliseconds());
-
-
-
-
-
-
-
-
-
-
-
-let users = [
+let categories = [
   {
-    name: "Joe",
-    date_of_birth: "01.05.2008"
+    id: 1,
+    name: "Horror",
   },
   {
-    name: "Jake",
-    date_of_birth: "12.09.2009"
+    id: 2,
+    name: "Comedy",
   },
   {
-    name: "Jim",
-    date_of_birth: "08.01.2011"
+    id: 3,
+    name: "Roman",
+  },
+];
+
+let films = [
+  {
+    id: 1,
+    catId: 1,
+    name: "Zvonok",
+    duration: "2:30:00",
   },
   {
-    name: "Jane",
-    date_of_birth: "23.06.2000"
+    id: 2,
+    catId: 2,
+    name: "3 lishniy",
+    duration: "1:30:00",
   },
   {
-    name: "John",
-    date_of_birth: "30.01.2007"
+    id: 3,
+    catId: 3,
+    name: "3 metra",
+    duration: "1:45:00",
+  },
+];
+
+let hall = [
+  {
+    id: 1,
+    places: 20,
   },
   {
-    name: "Joshua",
-    date_of_birth: "20.11.2012"
+    id: 2,
+    places: 30,
   },
-]
+  {
+    id: 3,
+    places: 25,
+  },
+];
+
+let sessions = [
+  {
+    id: 1,
+    filmId: 1,
+    hallId: 2,
+    startDate: "2026-06-25T10:00:00.686Z",
+    endDate: "2026-06-25T12:30:00.686Z",
+    places: 15,
+  },
+  {
+    id: 2,
+    filmId: 1,
+    hallId: 3,
+    startDate: "2026-06-25T10:00:00.686Z",
+    endDate: "2026-06-25T12:30:00.686Z",
+    places: 10,
+  },
+  {
+    id: 3,
+    filmId: 2,
+    hallId: 3,
+    startDate: "2026-06-25T10:00:00.686Z",
+    endDate: "2026-06-25T12:30:00.686Z",
+    places: 10,
+  },
+];
+
+
+function createSession(filmId, hallId, placesCount, startDate) {
+
+  let currentFilm = films.find(item => item.id === filmId)
+
+  if(!currentFilm) {
+    let str = ""
+    films.forEach(item => {
+      str += `id: ${item.id}, name: ${item.name} \n`
+    })
+    console.log(`Такого фильма не существует, выберите другой фильм`)
+    console.table(films)
+    return
+  } 
+
+
+
+}
+createSession(836217356271, 3, 20, "2026-06-25T10:00:00.686Z") // "2026-06-26T12:30:00.686Z"
