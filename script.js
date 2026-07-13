25b48b4401eaf2ffdb05655ae47d5cac25695c39
rukiakahramon-afk <rukiaturgunova32@gmail.coom>
2026-07-13 13:36:12 +0600

    test

--- script.js
+++ script.js
@@ -9,7 +9,7 @@ let categories = [
   },
   {
     id: 3,
-    name: "Roman",
+    name: "Novel",
   },
 ];
 
@@ -17,19 +17,19 @@ let films = [
   {
     id: 1,
     catId: 1,
-    name: "Zvonok",
+    name: "The ring",
     duration: "2:30:00",
   },
   {
     id: 2,
     catId: 2,
-    name: "3 lishniy",
+    name: "Ted 2",
     duration: "1:30:00",
   },
   {
     id: 3,
     catId: 3,
-    name: "3 metra",
+    name: "3 meters above the heaven",
     duration: "1:45:00",
   },
 ];
@@ -76,23 +76,96 @@ let sessions = [
   },
 ];
 
-
 function createSession(filmId, hallId, placesCount, startDate) {
 
-  let currentFilm = films.find(item => item.id === filmId)
+  let currentFilm = films.find(item => item.id === filmId);
 
   if (!currentFilm) {
-    let str = ""
-    films.forEach(item => {
-      str += `id: ${item.id}, name: ${item.name} \n`
-    })
-    console.log(`Такого фильма не существует, выберите другой фильм`)
-    console.table(films)
-    return
+    console.log("This movie does not exist!");
+    console.table(films);
+    return;
   }
 
-  let hello = ''
-  const o ="hi"
 
+  let currentHall = hall.find(item => item.id === hallId);
+
+  if (!currentHall) {
+    console.log("This hall does not exist!");
+    console.table(hall);
+    return;
+  }
+
+
+  if (placesCount > currentHall.places) {
+    console.log("There are not enough places!");
+    console.table(currentHall);
+    return;
+  }
+
+
+  let start = new Date(startDate);
+
+
+  let time = currentFilm.duration.split(":");
+
+  let hours = Number(time[0]);
+  let minutes = Number(time[1]);
+  let seconds = Number(time[2]);
+
+
+  let duration =
+    hours * 3600000 +
+    minutes * 60000 +
+    seconds * 1000;
+
+
+  let end = new Date(start.getTime() + duration);
+
+
+
+  let isBusy = sessions.some(session => {
+
+    if (session.hallId !== hallId) {
+      return false;
+    }
+
+
+    let oldStart = new Date(session.startDate);
+    let oldEnd = new Date(session.endDate);
+
+
+    return start < oldEnd && end > oldStart;
+  });
+
+
+
+  if (isBusy) {
+    console.log("No available time in this hall!");
+    console.table(
+      sessions.filter(session => session.hallId === hallId)
+    );
+    return;
+  }
+
+
+
+  let newSession = {
+    id: sessions.length + 1,
+    filmId: filmId,
+    hallId: hallId,
+    startDate: start.toISOString(),
+    endDate: end.toISOString(),
+    places: placesCount
+  };
+
+
+  sessions.push(newSession);
+
+
+  console.log("Session successfully created!");
+
+  console.table(sessions);
 }
-createSession(836217356271, 3, 20, "2026-06-25T10:00:00.686Z") // "2026-06-26T12:30:00.686Z"
+
+
+createSession( 2, 2, 30,"2026-06-26T10:00:00.686Z");
