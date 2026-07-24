68bc524c2bdbdb25b5892e1433bacd5e42c301e4
scDjanGo <scdjango229101@gmail.com>
2026-07-24 19:57:27 +0600

    add

--- script.js
+++ script.js
@@ -1,44 +1,35 @@
-let users = {
-    user_1: {
-        name: "Joe",
-        age: 30,
-        salary: 30000
-    },
-    user_2: {
-        name: "Joe",
-        age: 30,
-        salary: 37000
-    },
-    user_3: {
-        name: "Joe",
-        age: 30,
-        salary: 3000
-    },
-};
 
 
 
 
-let result = JSON.stringify(users, (key, value) => {
-    if(key === "") return value
-
-    console.log(value.salary >= 30000);
-     
 
 
-    if(typeof value === "object" && value.salary >= 30000) {
-        return value
-    }else if(typeof value !== "object") {
-        return value
+
+
+
+
+
+
+
+
+function setSettings(settings) {
+
+    const currentSettings = JSON.parse(localStorage.getItem("settings"))
+
+    for(let key in currentSettings) {
+        if(key in settings) {
+            currentSettings[key] = settings[key]
+        }
     }
 
 
-    
+    localStorage.setItem("settings", JSON.stringify(currentSettings))
 
+    console.log("Настройки были сохранены")
 
-}, 3)
+}
 
-
-
-
-console.log(result);
+setSettings({
+    theme: "light",
+    currency: "RUB"
+})
